9fe525ab8a1ba8ee25e07f6926bb9ec68d5c593f
mewsiof <mewsiof@gmail.com>
2018-03-21 15:57:24 -0600

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,2 +1,13 @@
 from lxml import html
 import requests
+
+
+class Scraper:
+  def setReqi(website):
+    page = requests.get(website)
+    tree = html.fromstring(page.content)
+
+  def getInfo(tag, dataitm):
+    data = tree.xpath('//' + tag '[@class=' + dataitm ']/text()')
+
+  
