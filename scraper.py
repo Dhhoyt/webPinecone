097b46498e8555bf279149774781ec3b9bda61e8
mewsiof <mewsiof@gmail.com>
2018-03-21 18:16:06 -0600

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,9 +3,22 @@ import requests
 
 
 class Scraper:
+  
   def getInfo(website, tag, dataitem):
     page = requests.get(website)
     tree = html.fromstring(page.content)
     data = tree.xpath('//' + tag '[@class=' + dataitem ']/text()')
-
-  
+    info = [website, tag, dataitem]
+    return info
+    
+  def getPage():
+    pagei = getInfo.info[0]
+    return pagei
+    
+  def getTag():
+    tagi = getInfo.info[1]
+    return tagi
+    
+  def getDataIt():
+   datait = getInfo.info[2]
+   return datait
