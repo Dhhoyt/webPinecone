194744a1f008cd0f4a343de4aac860dec53e1c6e
mewsiof <mewsiof@gmail.com>
2018-03-21 20:31:28 -0600

    Update websp.py

--- websp.py
+++ websp.py
@@ -1,1 +1,12 @@
+from lxml import html
+import requests
 
+def showLogo():
+  print("welcome to webPinecone!")
+website = input("Enter a website you want to scrape")
+tag = input("Enter the tag(s)")
+dataitem = input ("Enter a data item")
+
+scrinf = Scraper.getInfo(website, tag, dataitem)
+
+print(scrinf)
